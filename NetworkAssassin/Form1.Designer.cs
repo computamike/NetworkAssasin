92a7372454d25228bef85fd442fcffb8656c22dd
Mike Hingley <Computa_Mike@hotmail.com>
2015-02-26 23:55:49 +0000

    Assing a RESX resource file, contianing the Agent 47 graphic

---
+++ NetworkAssassin/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace NetworkAssassin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(569, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Agent 47 - Network Assasin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "This application will kill and resurrect any active network \r\nconnections.\r\n\r\nWhe" +
+    "n the application starts, a snapshot is taken ot the connectivity \r\nstate of all" +
+    " network connections";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 335);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "640509-040147";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NetworkAssassin/Form1.cs
@@ -0,0 +1,89 @@
+﻿using ROOT.CIMV2.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkAssassin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool Enabled = true; 
+        private Dictionary<string, int> NetworkStates = new Dictionary<string,int>();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in NetworkStates.Keys )
+            {
+                if (NetworkStates[item] == 2)
+                {// Kill it with FIRE
+                    SelectQuery query = new SelectQuery("Win32_NetworkAdapter", "DeviceID="+item.ToString() );
+                    ManagementObjectSearcher search = new ManagementObjectSearcher(query);
+                 
+                    foreach (var adapt in search.Get())
+                    {
+                        
+                        ManagementObject adapter =  (ManagementObject)adapt;
+                        if (Enabled)
+                        { adapter.InvokeMethod("Disable", null); }
+                        else
+                        { adapter.InvokeMethod("Enable", null); }
+                    }
+                }
+            }
+            Enabled = !Enabled;
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            ColorMatrix cm = new ColorMatrix();
+            cm.Matrix33 = 0.55f;
+            ImageAttributes ia = new ImageAttributes();
+            ia.SetColorMatrix(cm);
+            var x = this.Width - Properties.Resources.NetworkAssasin.Width;
+            e.Graphics.DrawImage(Properties.Resources.NetworkAssasin, new Rectangle(x, 0, Properties.Resources.NetworkAssasin.Width, Properties.Resources.NetworkAssasin.Height), 0, 0, Properties.Resources.NetworkAssasin.Width, Properties.Resources.NetworkAssasin.Height, GraphicsUnit.Pixel, ia);
+
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+            // Get Network States
+            NetworkStates.Clear();
+            NetworkStates = GetDeviceSettings();
+
+
+        }
+
+        private Dictionary<string, int> GetDeviceSettings()
+        {
+            Dictionary<string, int> NetworkStates = new Dictionary<string, int>();
+            SelectQuery AllNetworkAdapterQuery = new SelectQuery("Win32_NetworkAdapter");
+            ManagementObjectSearcher search = new ManagementObjectSearcher(AllNetworkAdapterQuery);
+            var res = search.Get();
+
+            foreach (ManagementObject result in search.Get())
+            {
+                NetworkAdapter adapter = new NetworkAdapter(result);
+
+                NetworkStates.Add(adapter.DeviceID, adapter.NetConnectionStatus);
+            }
+            return NetworkStates;
+
+        }
+    }
+}
